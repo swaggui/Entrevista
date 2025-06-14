2962e966d8bc2fa3cd8b825e6c12ac902f5b4594
swaggui <guilhermepreinaldo@gmail.com>
2025-06-14 19:40:24 -0300

    Inicio do projeto: configurações iniciais, rotas mapeadas e classe de validação

---
+++ src/MapaDeRotas.php
@@ -0,0 +1,38 @@
+<?php
+
+class MapaDeRotas
+{
+    private const MAPA_DE_ESTADOS = [
+        'RS' => ['SC'],
+        'SC' => ['RS', 'PR'],
+        'PR' => ['SC', 'SP', 'MS'],
+        'SP' => ['PR', 'RJ', 'MG', 'MS'],
+        'RJ' => ['SP', 'MG', 'ES'],
+        'MG' => ['SP', 'RJ', 'ES', 'GO', 'BA', 'DF'],
+        'ES' => ['RJ', 'MG', 'BA'],
+        'BA' => ['ES', 'MG', 'GO', 'TO', 'PI', 'PE', 'AL', 'SE'],
+        'MS' => ['PR', 'SP', 'MG', 'GO', 'MT'],
+        'GO' => ['MG', 'DF', 'MT', 'TO', 'BA', 'MS'],
+        'MT' => ['RO', 'AM', 'PA', 'TO', 'GO', 'MS'],
+        'RO' => ['AC', 'AM', 'MT'],
+        'AM' => ['RO', 'RR', 'PA', 'MT', 'AC'],
+        'PA' => ['AM', 'RR', 'AP', 'MA', 'TO', 'MT'],
+        'RR' => ['AM', 'PA'],
+        'AP' => ['PA'],
+        'MA' => ['PA', 'TO', 'PI'],
+        'TO' => ['PA', 'MA', 'PI', 'BA', 'GO', 'MT'],
+        'PI' => ['MA', 'CE', 'PE', 'BA', 'TO'],
+        'CE' => ['PI', 'PE', 'RN', 'PB'],
+        'RN' => ['CE', 'PB'],
+        'PB' => ['RN', 'CE', 'PE'],
+        'PE' => ['PB', 'CE', 'PI', 'BA', 'AL'],
+        'AL' => ['PE', 'BA', 'SE'],
+        'SE' => ['AL', 'BA'],
+        'DF' => ['GO']
+    ];
+
+    public function getVizinhos(string $estado): ?array
+    {
+        return self::MAPA_DE_ESTADOS[$estado] ?? null;
+    }
+}
---
+++ src/ValidadorDeRotas.php
@@ -0,0 +1,28 @@
+<?php
+class ValidadorDeRotas
+{
+    private MapaDeRotas $mapaDeRotas;
+
+    public function __construct(MapaDeRotas $mapaDeRotas)
+    {
+        $this->mapaDeRotas = $mapaDeRotas;
+    }
+
+    public function isRotaValida(string $rota): bool
+    {
+        $estados = explode(',', $rota);
+
+        for ($i = 0; $i < count($estados) - 1; $i++) {
+            $estadoAtual = trim($estados[$i]);
+            $proximoEstado = trim($estados[$i + 1]);
+
+            $vizinhos = $this->mapaDeRotas->getVizinhos($estadoAtual);
+
+            if ($vizinhos === null || !in_array($proximoEstado, $vizinhos)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
